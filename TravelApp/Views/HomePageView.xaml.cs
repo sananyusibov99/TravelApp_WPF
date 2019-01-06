cdc73af3f069e99c36ad493ba5749d4bcff95155
Sanan <sananyusibov99@gmail.com>
2019-01-06 19:45:30 +0400

    Added HomePageView and HomeMenuPageView

---
+++ TravelApp/Views/HomePageView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePageView.xaml
+    /// </summary>
+    public partial class HomePageView : Window
+    {
+        public HomePageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
